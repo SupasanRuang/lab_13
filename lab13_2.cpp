c8a02fd58e77a6ee41e9c0af468a7acb14eca015
SupasanRuang <supasan_ruang@cmu.ac.th>
2021-12-24 14:37:42 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,33 @@
 #include<iostream>
 using namespace std;
 
+int  gcd(int ,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+int  gcd(int m,int n)
+{
+    int t;
+    if(m<n)
+    {
+        t=m;
+        m=n;
+        n=t;
+    }
+    if(m%n==0)
+    {
+        return n;
+    }
+    else 
+    {
+        return gcd(n,m%n);
+    }
+
+
+
+
 }
