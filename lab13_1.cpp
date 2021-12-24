2c16c8cc38ab30a9b8b7d0b218ea3c70ff7cd845
SupasanRuang <supasan_ruang@cmu.ac.th>
2021-12-24 13:51:24 +0700

    lab12_1_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,34 @@
 #include<iostream>
 using namespace std;
 
+
+
+
+long long int  fibonacci(long long int k );
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+
+}
+long long int  fibonacci(long long int k)
+{
+    if(k==0)
+    {
+        return 0;
+    }
+    else if(k==1)
+    {
+        return 1;
+    }
+    else 
+    {
+
+        return fibonacci(k-1)+fibonacci(k-2);
+    }
+
+
+
 }
